c023c83b888a3c490f5da8697f6f54a51d69b0c9
Dowin452 <barsamvergil051@gmail.com>
2025-03-31 13:26:32 +0330

    Delete install_c(python).sh

--- install_c(python).sh
+++
@@ -1,8 +0,0 @@
-sudo pacman -Syu
-sudo pacman -S git gcc
-sudo pacman -S python python-pip
-pip install windows-curses
-git clone https://github.com/Dowin452/iClass
-cd iClass
-chmod +x iclass.py
-echo "Type ./iclass to run the program"
